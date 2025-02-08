028cbad76b77ed397e4a45ee62d5b4a24406de0f
balion <amybalion533@gmail.com>
2025-02-08 08:34:18 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ int main() {
         // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
         // breakpoint for you, but you can always add more by pressing
         // <shortcut actionId="ToggleLineBreakpoint"/>.
-        std::cout << "amyaya test ccccc" << i << std::endl;
+        std::cout << "amyaya test ccccc amya again" << i << std::endl;
     }
 
     return 0;
