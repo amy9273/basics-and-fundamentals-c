8bf6b89155b2d356efa50917b61c7f72f0623420
balion <amybalion533@gmail.com>
2025-02-08 11:21:28 +0800

    s

--- main.cpp
+++ main.cpp
@@ -1,98 +1,25 @@
-/*  Basics and Fundamentals of C++
- *  Balion, Angel Amaya B.
- */
-
-
 #include <iostream>
-#include <limits>
-
 using namespace std;
+// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+int main() {
+    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
+    // <b>lang</b> variable name to see how CLion can help you rename it.
+    auto lang = "C++";
+    cout << "Hello and welcome to " << lang << "!\n";
 
-//void function for validating input
-void ValidateInput(int &num, bool &valid, int max){
-    cin>>num;
-    if(cin.fail()){
-        cin.clear();
-        cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-        cout << "\tInvalid input. Please enter an integer."<<endl<<endl;
-        valid = false;
-    }else if(max != 0 && (num > max || num <= 0)){ //max == 0 means walang maximum na nakaset
-        cout << "\tInvalid input. Please enter an integer between 1 and "<< max <<"."<<endl<<endl;
-        valid = false;
-    }else{
-        valid = true;
+    for (int i = 1; i <= 5; i++) {
+        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
+        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
+        // breakpoint for you, but you can always add more by pressing
+        // <shortcut actionId="ToggleLineBreakpoint"/>.
+        cout << "amyaya test ccccc amya again" << i << endl;
+        cout << "New Line meme" <<endl;
     }
-
+    return 0;
 }
 
-int main(){
-    int fnum, snum;
-    bool valid = true;
-
-    cout<<"\t\t\tMerge Descending Order"<<endl<<endl;
-
-    //First Array
-    do{ //get array size
-        cout<<"Enter number of elements for the first array (10 max): ";
-        ValidateInput(fnum, valid, 10);
-        while ((getchar()) != '\n');
-    }while(!valid);
-    int firstArray[fnum];
-    cout<<"Enter "<< fnum <<" elements of the first array:"<<endl;
-    for(int i = 0; i < fnum; i++){ //get elements of the array
-        do{
-            cout<<"["<<i+1<<"]: ";
-            ValidateInput(firstArray[i], valid, 0);
-            while ((getchar()) != '\n');
-        }while(!valid);
-    }
-
-    cout<<endl<<endl;
-
-    //Second Array
-    do{ //get array size
-        cout<<"Enter number of elements for the second array (10 max): ";
-        ValidateInput(snum, valid, 10);
-        while ((getchar()) != '\n');
-    }while(!valid);
-    int secondArray[snum];
-    cout<<"Enter "<< snum <<" elements of the second array: \n"; //get array elements
-    for(int i = 0; i < snum; i++){
-        do{
-            cout<<"["<<i+1<<"]: ";
-            ValidateInput(secondArray[i], valid, 0);
-            while ((getchar()) != '\n');
-        }while(!valid);
-    }
-
-    //Combine First and Second Array
-    int totalNum = fnum + snum;
-    int mergeArray[totalNum];
-    for(int i = 0; i < totalNum; i++){
-        if(i < fnum){
-            mergeArray[i] = firstArray[i];
-        }else{
-            mergeArray[i] = secondArray[i-fnum];
-        }
-    }
-
-    //bubble sort the merge array in descending order
-    for(int i = 0; i < totalNum; i++) {
-        int prev;
-		for(int j = i+1; j < totalNum; j++) {
-            //if the next element is greater than the current element, switch
-			if(mergeArray[i] < mergeArray[j]) {
-				prev = mergeArray[i];
-				mergeArray[i] = mergeArray[j];
-				mergeArray[j] = prev;
-			}
-		}
-	}
-    cout<<"hihi";
-
-    //print
-    cout<<endl<<endl<<"Descending Merge: ";
-    for(int i = 0; i < totalNum; i++){
-        cout<<mergeArray[i]<<" ";
-    }
-}
+// TIP See CLion help at <a
+// href="https://www.jetbrains.com/help/clion/">jetbrains.com/help/clion/</a>.
+//  Also, you can try interactive lessons for CLion by selecting
+//  'Help | Learn IDE Features' from the main menu.
