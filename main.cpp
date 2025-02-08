c551a6f633a6b4178995a64c4b4e60392a7a128e
balion <amybalion533@gmail.com>
2025-02-08 09:41:38 +0800

    second commit

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 using namespace std;
-// TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 int main() {
-    // TIP Press <shortcut actionId="RenameElement"/> when your caret is at the
-    // <b>lang</b> variable name to see how CLion can help you rename it.
+
     auto lang = "C++";
     cout << "Hello and welcome to " << lang << "!\n";
 
     for (int i = 1; i <= 5; i++) {
-        // TIP Press <shortcut actionId="Debug"/> to start debugging your code.
-        // We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/>
-        // breakpoint for you, but you can always add more by pressing
-        // <shortcut actionId="ToggleLineBreakpoint"/>.
+
         cout << "amyaya test ccccc amya again" << i << endl;
-        cout << "New Line meme" <<endl;
+        cout << "New Line meme first branch amaya" <<endl;
     }
     return 0;
 }
